c8d8e58888ff4d343012917e0184779f58dd1ac4
Anish Saha <asaha@berkeley.edu>
2017-01-05 19:32:37 -0800

    Update waypoints.js

--- js/waypoints.js
+++ js/waypoints.js
@@ -1,6 +1,6 @@
 /*
 jQuery Waypoints - v1.1.7
-Copyright (c) 2011-2012 Caleb Troughton
+Credit: Caleb Troughton - Copyright (c) 2011-2012
 Dual licensed under the MIT license and GPL license.
 https://github.com/imakewebthings/jquery-waypoints/blob/master/MIT-license.txt
 https://github.com/imakewebthings/jquery-waypoints/blob/master/GPL-license.txt
